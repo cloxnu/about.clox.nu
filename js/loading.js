2889257141ca776775c16fce479ae460dc388d93
Sidney Liu <cloxnu@outlook.com>
2021-02-03 15:17:25 +0800

    Update home right-arrow

--- js/loading.js
+++ js/loading.js
@@ -4,6 +4,7 @@ function loaded() {
         window.setTimeout(function () {
             document.body.classList.remove('loading')
             document.getElementById('display-div').classList.add('appear')
+            document.getElementById('right-arrow-wrapper').classList.add('appear')
         }, 1000)
 
     }, 1000)
