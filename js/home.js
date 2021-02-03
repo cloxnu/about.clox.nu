e6caeb115c056ea4d1fb084fd6f153c73ff7ae3d
Sidney Liu <cloxnu@outlook.com>
2021-02-03 23:18:14 +0800

    Update home

--- js/home.js
+++ js/home.js
@@ -1,3 +1,12 @@
+const Level = {
+    MainMenu: 'MainMenu'
+}
+const maxPage = {
+    'MainMenu': 2
+}
+let currentLevel = Level.MainMenu
+let currentPage = 0
+
 // dash switch animation
 function switch_display() {
     const ele = document.getElementById('bg-border')
@@ -5,7 +14,7 @@ function switch_display() {
     const bg_position = style.backgroundPosition
     const bg_position_x1 = style.backgroundPositionX.split(',')[0]
     const bg_position_x2 = style.backgroundPositionX.split(',')[1]
-    console.log(bg_position)
+    // console.log(bg_position)
     document.body.style.setProperty('--dash-bg-position-x1-from', bg_position_x1)
     document.body.style.setProperty('--dash-bg-position-x2-from', `calc(${bg_position_x2} + min(4vw, 4vh) * 20)`)
     // document.body.style.setProperty('--dash-bg-position-x1-to', `calc(${bg_position_x1} + min(4vw, 4vh) * 20)`)
@@ -16,4 +25,8 @@ function switch_display() {
         ele.classList.remove('switch-display')
     }, 2000)
     console.log(bg_position)
+}
+
+function right_arrow_clicked() {
+    console.log("clicked")
 }
--- js/loading.js
+++ js/loading.js
@@ -4,8 +4,10 @@ function loaded() {
         window.setTimeout(function () {
             document.body.classList.remove('loading')
             document.getElementById('display-div').classList.add('appear')
-            document.getElementById('right-arrow-wrapper').classList.add('appear')
         }, 1000)
+        window.setTimeout(function () {
+            document.getElementById('right-arrow-wrapper').classList.add('appear')
+        }, 2000)
 
     }, 1000)
 
