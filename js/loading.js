25d22b3716afe8e335ac7c18754c691b32898b3e
Sidney Liu <cloxnu@outlook.com>
2021-02-04 11:16:20 +0800

    Add welcome-words pause

--- js/loading.js
+++ js/loading.js
@@ -3,6 +3,7 @@ function loaded() {
         bg_border_switch_display()
         window.setTimeout(() => {
             document.body.classList.remove('loading')
+            document.getElementById('welcome-words').classList.remove('paused')
             display_div_appear()
         }, 1000)
         window.setTimeout(() => {
