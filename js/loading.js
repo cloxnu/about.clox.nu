d28657b4ef38fb9877cd9e1a88b32097155b72c0
cloxnu <o18089541818@outlook.com>
2021-02-07 13:58:48 +0800

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,17 +1,14 @@
 function loaded() {
+    bg_border_switch_display()
     window.setTimeout(() => {
-        bg_border_switch_display()
-        window.setTimeout(() => {
-            document.body.classList.remove('loading')
-            document.getElementById('welcome-words').classList.remove('paused')
-            document.getElementById('welcome-words').classList.add('show')
-            display_div_appear()
-        }, 1000)
-        window.setTimeout(() => {
-            arrow_appear()
-        }, 2000)
-
+        document.body.classList.remove('loading')
+        document.getElementById('welcome-words').classList.remove('paused')
+        document.getElementById('welcome-words').classList.add('show')
+        display_div_appear()
     }, 1000)
+    window.setTimeout(() => {
+        arrow_appear()
+    }, 2000)
 
 }
 
